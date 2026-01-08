97f4c14a1ee9debffe0c40861270b1b6caa4c767
niclas <niclas@hedhman.org>
2026-01-08 16:46:53 +0800

    ...

--- first/main.c
+++ first/main.c
@@ -6,77 +6,86 @@
 #define PINNO(pin) (pin & 255)
 #define PINBANK(pin) (pin >> 8)
 
-struct rcc {
-  volatile uint32_t CR, PLLCFGR, CFGR, CIR, AHB1RSTR, AHB2RSTR, AHB3RSTR,
-      RESERVED0, APB1RSTR, APB2RSTR, RESERVED1[2], AHB1ENR, AHB2ENR, AHB3ENR,
-      RESERVED2, APB1ENR, APB2ENR, RESERVED3[2], AHB1LPENR, AHB2LPENR,
-      AHB3LPENR, RESERVED4, APB1LPENR, APB2LPENR, RESERVED5[2], BDCR, CSR,
-      RESERVED6[2], SSCGR, PLLI2SCFGR;
+struct rcc
+{
+    volatile uint32_t CR, PLLCFGR, CFGR, CIR, AHB1RSTR, AHB2RSTR, AHB3RSTR,
+                      RESERVED0, APB1RSTR, APB2RSTR, RESERVED1[2], AHB1ENR, AHB2ENR, AHB3ENR,
+                      RESERVED2, APB1ENR, APB2ENR, RESERVED3[2], AHB1LPENR, AHB2LPENR,
+                      AHB3LPENR, RESERVED4, APB1LPENR, APB2LPENR, RESERVED5[2], BDCR, CSR,
+                      RESERVED6[2], SSCGR, PLLI2SCFGR;
 };
+
 #define RCC ((struct rcc *) 0x40023800)
 
-struct gpio {
-  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
-};
-#define GPIO(bank) ((struct gpio *) (0x40020000 + 0x400 * (bank)))
+typedef struct
+{
+    volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
+} gpio_t;
+
+#define GPIO(bank) ((gpio_t *) (0x40020000 + 0x400 * (bank)))
 
 // Enum values are per datasheet: 0, 1, 2, 3
 enum { GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG };
 
-static inline void gpio_set_mode(uint16_t pin, uint8_t mode) {
-  struct gpio *gpio = GPIO(PINBANK(pin));  // GPIO bank
-  int n = PINNO(pin);                      // Pin number
-  gpio->MODER &= ~(3U << (n * 2));         // Clear existing setting
-  gpio->MODER |= (mode & 3) << (n * 2);    // Set new mode
+static inline void gpio_set_mode(uint16_t pin, uint8_t mode)
+{
+    uint32_t pin_number = PINNO(pin); // Pin number
+    gpio_t* gpio = GPIO(PINBANK(pin_number)); // GPIO bank
+    uint32_t bits = pin_number << 1;
+    gpio->MODER &= ~(3U << bits); // Clear existing setting
+    gpio->MODER |= (uint32_t)(mode & 3) << bits; // Set new mode
 }
 
-static inline void gpio_write(uint16_t pin, bool val) {
-  struct gpio *gpio = GPIO(PINBANK(pin));
-  gpio->BSRR = (1U << PINNO(pin)) << (val ? 0 : 16);
+static inline void gpio_write(uint16_t pin, bool val)
+{
+    gpio_t* gpio = GPIO(PINBANK(pin));
+    gpio->BSRR = (1U << PINNO(pin)) << (val ? 0 : 16);
 }
 
-static inline void spin(volatile uint32_t count) {
-  while (count--) (void) 0;
+static inline void spin(volatile uint32_t count)
+{
+    while (count--) (void)0;
 }
 
-int main(void) {
-  uint16_t led = PIN('C', 13);            // Blue LED
-  RCC->AHB1ENR |= BIT(PINBANK(led));     // Enable GPIO clock for LED
-  gpio_set_mode(led, GPIO_MODE_OUTPUT);  // Set blue LED to output mode
-  for (;;) {
-    gpio_write(led, true);
-    spin(999999);
-    gpio_write(led, false);
-    spin(999999);
-  }
-  return 0;
+int main(void)
+{
+    uint16_t led = PIN('C', 13); // Blue LED
+    RCC->AHB1ENR |= BIT(PINBANK(led)); // Enable GPIO clock for LED
+    gpio_set_mode(led, GPIO_MODE_OUTPUT); // Set blue LED to output mode
+    for (;;)
+    {
+        gpio_write(led, true);
+        spin(999999);
+        gpio_write(led, false);
+        spin(999999);
+    }
+    return 0;
 }
 
 // Startup code
-__attribute__((naked,noreturn)) void _reset(void) {
+__attribute__((noreturn)) void _reset(void)
+{
+    extern long _sbss, _ebss, _sdata, _edata, _sidata;
+    for (long* dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+    for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
 
-  extern long _sbss, _ebss, _sdata, _edata, _sidata;
-  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
-  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
-
-  main();
-  for (;;) (void) 0;  // Infinite loop
+    main();
+    for (;;) (void)0; // Infinite loop
 }
 
-extern void _estack(void);  // Defined in link.ld
+extern void _estack(void); // Defined in link.ld
 
 void _nmi(void)
 {
-  for (;;) (void) 0;  // Infinite loop
+    for (;;) (void)0; // Infinite loop
 }
 
 void _hardfault(void)
 {
-  for (;;) (void) 0;  // Infinite loop
+    for (;;) (void)0; // Infinite loop
 }
 
 // 16 standard and 91 STM32-specific handlers
 __attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
-  _estack, _reset, _nmi, _hardfault
+    _estack, _reset, _nmi, _hardfault
 };
-
